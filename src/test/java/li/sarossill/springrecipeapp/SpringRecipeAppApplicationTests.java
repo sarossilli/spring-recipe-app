bbd6b75a1c2adcc51ebbe1b6746578fc66031813
Sam Rossilli <Sarossilli@gmail.com>
2022-01-19 08:18:05 -0700

    Initial commit

---
+++ src/test/java/li/sarossill/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package li.sarossill.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
